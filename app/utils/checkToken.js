a2c9cb73a830c7d42c6c94c03d48758559eaae3a
eyalyehiely <eyalwork0@gmail.com>
2024-10-21 02:44:08 +0900

    update checkToken

--- app/utils/checkToken.js
+++ app/utils/checkToken.js
@@ -1,37 +1,15 @@
-import { useEffect } from 'react';
-import { useRouter } from 'next/navigation';
-import {jwtDecode} from 'jwt-decode';  // Correct import
+export default function checkToken() {
+  const token = localStorage.getItem('authToken');
+  
+  if (!token) {
+    // Redirect to /login if no auth token is found
+    localStorage.removeItem('authToken');
+    window.location.href = '/login';
+    
 
-const checkToken = () => {
-    const router = useRouter();
+  } else {
+    // Token exists, allow the user to continue browsing
+    console.log('User is authenticated, continue browsing');
+  }
+}
 
-  useEffect(() => {
-    const token = localStorage.getItem('authToken') ? JSON.parse(localStorage.getItem('authToken')).access : null;
-
-    if (token) {
-      try {
-        const decodedToken = jwtDecode(token);
-        const currentTime = Date.now() / 1000;
-
-        // Check if the token is expired
-        if (decodedToken.exp < currentTime) {
-          localStorage.removeItem('authToken');
-          router.push('/login');  // Redirect to home if the token is expired
-        }
-
-        // Check if the user is not a 'Company'
-        if (decodedToken.username == null) {
-          router.push(-1);  // navigate back to the previous page
-        }
-      } catch (error) {
-        console.error('Invalid token:', error);
-        localStorage.removeItem('authTokens');
-        router.push('/login');  // Redirect to home if the token is invalid
-      }
-    } else {
-      router.push('/login');  // Redirect to home if no token is found
-    }
-  }, [router]);  // Runs only once on mount
-};
-
-export default checkToken;
