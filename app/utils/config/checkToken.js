10a855d9d63d1c3744cfa1f4d10a30299ab79da7
eyalyehiely <eyalwork0@gmail.com>
2024-10-23 02:39:33 +0900

    upgrade check token func

--- app/utils/config/checkToken.js
+++ app/utils/config/checkToken.js
@@ -1,15 +1,24 @@
+import Swal from "sweetalert2";
+
 export default function checkToken() {
   const token = localStorage.getItem('authToken');
-  
-  if (!token) {
-    // Redirect to /login if no auth token is found
-    localStorage.removeItem('authToken');
-    window.location.href = '/login';
-    
 
+  if (!token) {
+    // Show an alert before redirecting to /login
+    window.location.href = '/login';
+    localStorage.removeItem('authToken');
+    Swal.fire({
+      icon: "error",
+      title: "Oops...",
+      text: "Something went wrong!",
+      footer: '<a href="#">Why do I have this issue?</a>'
+    }).then(() => {
+      // Redirect to /login if no auth token is found
+      
+     
+    });
   } else {
     // Token exists, allow the user to continue browsing
     console.log('User is authenticated, continue browsing');
   }
 }
-
